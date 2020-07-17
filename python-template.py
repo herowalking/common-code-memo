4cd7235c159804683daaf6b5ee8bc95918e68447
Lincoln <herowalking@hotmail.com>
2020-07-17 15:52:11 +0800

    python template add: DFS, BFS, 二分查找

--- python-template.py
+++ python-template.py
@@ -33,30 +33,100 @@ def recursion(level, param1, param2, ...):
 
   # reverse the current level status if needed
 
-  # 分治代码模板
-  def divide_conquer(problem, param1, param2, ...):
-    # rerursion terminator
-    if problem is None:
-      print_result
-      return
-    # prepare data
-    data = prepare_data(problem)
-    subproblems = split_problem(problem, data)
-    # conquer subproblems
-    subresult1 = self.divide_conquer(subproblems[0], p1, ...)
-    subresult2 = self.divide_conquer(subproblems[1], p1, ...)
-    subresult3 = self.divide_conquer(subproblems[2], p1, ...)
-    ...
-    # process and generate the final result
-    result = process_result(subresult1, subresult2, subresult3, ...)
+# 分治代码模板
+def divide_conquer(problem, param1, param2, ...):
+  # rerursion terminator
+  if problem is None:
+    print_result
+    return
+  # prepare data
+  data = prepare_data(problem)
+  subproblems = split_problem(problem, data)
+  # conquer subproblems
+  subresult1 = self.divide_conquer(subproblems[0], p1, ...)
+  subresult2 = self.divide_conquer(subproblems[1], p1, ...)
+  subresult3 = self.divide_conquer(subproblems[2], p1, ...)
+  ...
+  # process and generate the final result
+  result = process_result(subresult1, subresult2, subresult3, ...)
 
-    # revert the current level states
+  # revert the current level states
 
-    # TreeNode
-    class TreeNode:
-      def __init__(self, val):
-        self.val = val
-        self.left, self.right = None, None
+# TreeNode
+class TreeNode:
+  def __init__(self, val):
+    self.val = val
+    self.left, self.right = None, None
+
+# DFS 代码 - 递归写法
+visited = set()
+
+def dfs(node, visited):
+  if node in visited: # terminator
+    # already visited
+    return
+  
+  visited.add(node)
+
+  # process current node
+  # ... # logic here
+  for next_node in node.children():
+    if not next_node in visited:
+      dfs(next_node, visited)
+
+# DFS 代码 - 非递归写法
+def DFS(self, tree):
+
+  if tree.root is None:
+    return []
+  
+  visited, stack = [], [tree.root]
+
+  while stack:
+    node = stack.pop()
+    visited.add(node)
+
+    process(node)
+    nodes = generate_related_nodes(node)
+    stack.push(nodes)
+  
+  # other processing work 
+  ...
+
+# BFS 代码
+def BFS(graph, start, end):
+
+  queue = []
+  queue.append([start])
+  visited.add(start)
+
+  while queue:
+    node = queue.pop()
+    visited.add(node)
+
+    process(node)
+    nodes = generate_related_nodes(node)
+    queue.push(nodes)
+  
+  # other processing work 
+  ...
+
+# 二分查找
+left, right = 0, len(array) - 1
+while (left <= right):
+  mid = (left + right) / 2
+  if array[mid] == target:
+    # find the target!! 
+    break or return result
+  elif array[mid] < target:
+    left = mid + 1
+  else:
+    right = mid - 1
+  
+
+
+
+  
 
 
   
