4a6c164d8c0f21417f6d6222453f4baff95b4088
Lincoln <herowalking@hotmail.com>
2020-08-12 16:38:54 +0800

    add sort.js

---
+++ sort.js
@@ -0,0 +1,48 @@
+// 冒泡排序
+function bubbleSort(arr) {
+  var len = arr.length;
+  for (let i = 0; i < len - 1; i++) {
+    for (let j = 0; j < len - 1 - i; j++) {
+      if (arr[j] > arr[j + 1]) {
+        var temp = arr[j + 1];
+        arr[j + 1] = arr[j];
+        arr[j] = temp;
+      }
+    }
+  }
+  return arr;
+}
+
+// 选择排序
+function selectionSort(arr) {
+  var len = arr.length;
+  var minIndex, temp;
+  for (let i = 0; i < len - 1; i++) {
+    minIndex = i;
+    for (let j = i + 1; j < len; j++) {
+      if (arr[j] < arr[minIndex]) {
+        minIndex = j;
+      }
+    }
+    temp = arr[i];
+    arr[i] = arr[minIndex];
+    arr[minIndex] = temp;
+  }
+  return arr;
+}
+
+// 插入排序
+function insertionSort(arr) {
+  var len = arr.length;
+  var preIndex, current;
+  for (let i = 1; i < len; i++) {
+    preIndex = i - 1;
+    current = arr[i];
+    while (preIndex >= 0 && arr[preIndex] > current) {
+      arr[preIndex + 1] = arr[preIndex];
+      preIndex--;
+    }
+    arr[preIndex + 1] = current;
+  }
+  return arr;
+}
