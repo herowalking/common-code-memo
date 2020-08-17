9e11c418f848903d5d125c80676f9fda63a085f8
Lincoln <herowalking@hotmail.com>
2020-08-17 14:51:05 +0800

    add mergeSort to sort.js

--- sort.js
+++ sort.js
@@ -68,3 +68,22 @@ const partition = (nums, left, right) => {
   [nums[pivot], nums[index - 1]] = [nums[index - 1], nums[pivot]];
   return index - 1;
 }
+
+// merge sort
+const mergeSort = (nums) => {
+  if (nums.length <= 1) return nums;
+  let mid = (Math.floor(nums.length / 2)), 
+      left = nums.slice(0, mid), 
+      right = nums.slice(mid);
+  return merge(mergeSort(left), mergeSort(right));
+} 
+
+const merge(left, right) => {
+  let result = [];
+  while (left.length && right.length) {
+    result.push(left[0] <= right[0] ? left.shift() : right.shift())
+  }
+  while (left.length) result.push(left.shift());
+  while (right.length) result.push(right.shift());
+  return result;
+}
