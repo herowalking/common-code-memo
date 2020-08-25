aa1eb7e0cfaf8243494cbfc4b3548044fdf4b840
Lincoln <herowalking@hotmail.com>
2020-08-25 16:02:53 +0800

    add myCall, myApply, myBind to common.js

--- common.js
+++ common.js
@@ -13,3 +13,52 @@ function deepCopy(p, c) {
   }
   return c;
 }
+
+// 1. 实现一个call函数 
+// 思路：将要改变this指向的方法挂到目标this上执行并返回
+Function.prototype.myCall = function(context) {
+  if (typeof this !== 'function') {
+    throw new TypeError('not function');
+  }
+  context = context || window;
+  context.fn = this
+  let args = [...arguments].slice(1);
+  let result = context.fn(...args);
+  delete context.fn;
+  return result;
+}
+
+// 2. 实现一个apply函数 
+// 思路：将要改变this指向的方法挂到目标this上执行并返回
+Function.prototype.myApply = function(context) {
+  if (typeof this !== 'function') {
+    throw new TypeError('not function');
+  }
+  context = context || window;
+  context.fn = this;
+  let result;
+  if (arguments[1]) {
+    result = context.fn(...arguments[1])
+  } else {
+    result = context.fn();
+  }
+  delete context.fn;
+  return result;
+}
+
+// 3. 实现一个bind函数
+// 思路：类似call，但返回的是函数
+Function.prototype.myBind = function(context) {
+  if (typeof this !== 'function') {
+    throw new TypeError('not function');
+  }
+  let _this = this;
+  let args = [...arguments].slice(1);
+  return function F() {
+    if (this instanceof F) {
+      return new _this(...args, ...arguments);
+    } else {
+      return _this.apply(context, args.concat(...arguments));
+    }
+  }
+}
