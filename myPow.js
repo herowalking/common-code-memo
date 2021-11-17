0cb1607b2bc43e1d1cd41e24a3cee1294b52942f
Lincoln <herowalking@hotmail.com>
2021-11-17 11:36:00 +0800

    update myPow.js

--- myPow.js
+++ myPow.js
@@ -10,9 +10,10 @@
  * @param {number} n
  * @return {number}
  */
-var myPow = function(x, n) {
+var myPow = function (x, n) {
   if (n === 0) return 1;
   let nn = Math.abs(n);
-  let res = nn % 2 === 0 ? myPow(x * x, nn / 2) : myPow(x * x, (nn - 1) / 2) * x;
+  let res =
+    nn % 2 === 0 ? myPow(x * x, nn / 2) : myPow(x * x, (nn - 1) / 2) * x;
   return n < 0 ? 1 / res : res;
 };
